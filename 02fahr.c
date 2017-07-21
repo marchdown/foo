6fffe5ec70cfb4017c5dd2fdf949450ee107f79a
marchdown <marchdown@gmail.com>
2017-07-21 20:54:22 +0300

    floating point values

--- 02fahr.c
+++ 02fahr.c
@@ -5,18 +5,18 @@
 
 main()
 {
-  int fahr, celsius;
-  int lower, upper, step;
+  float fahr, celsius;
+  float lower, upper, step;
 
   lower = 0;
   upper = 300;
   step  = 20;
 
-  fahr = lower;
+  fahr = lower; // type coercion
 
   while (fahr <= upper) {
-    celsius = 5 * (fahr - 32) / 9;
-    printf("%d\t%d\n", fahr, celsius);
+    celsius = (fahr - 32) * (5./9);
+    printf("%.0f\t%.2f\n", fahr, celsius);
     fahr = fahr + step;
   }
 }
