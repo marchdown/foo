e0577b35279c4c80ee18037c0cac9716a3ba7381
marchdown <marchdown@gmail.com>
2017-07-27 03:02:51 +0300

    call-by-value example

---
+++ 11pow_call_by_value.c
@@ -0,0 +1,13 @@
+// int main(){
+//   int()
+//   gets() // how do I read a number ¡derp!
+//   return(0);
+// }
+#include <stdio.h>
+#include "pow_call_by_value.c"
+
+
+int main(){
+  printf("%f", pow(4, 7));
+  return(0);
+}
---
+++ pow_call_by_value.c
@@ -0,0 +1,16 @@
+// int main(){
+//   int()
+//   gets() // how do I read a number ¡derp!
+//   return(0);
+// }
+
+float pow(float base, int exp){
+  float res;
+  for(res = base; exp > 0; exp--){
+    res *= base;
+  }
+  return(res);
+}
+
+
+// uuhh how do I #include this file?
